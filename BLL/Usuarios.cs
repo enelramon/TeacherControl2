75d7cbee2d9b798d2013fe76c21025d3ed42c541
Blas <blasroniel@live.com>
2014-11-05 17:34:27 -0400

    Agregue  Clase Usuarios

---
+++ BLL/Usuarios.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DAL;
+
+namespace BLL
+{
+    public class Usuarios
+    {
+        public string Nombre { set; get; }
+        public string Clave { set; get; }
+        public int Tipo { set; get; }
+        public bool Activo { set; get; }
+
+        ConexionDb db = new ConexionDb();
+        DataTable dt = new DataTable();
+
+        public bool Insertar()
+        {
+            return db.EjecutarDB(string.Format("Insert into Usuarios(Nombre, Clave, Tipo, Activo) values ('" + Nombre + "', '" + Clave + "', " + Tipo + ", '" + Activo + "')"));
+        }
+
+        public bool IniciarSesion()
+        {
+            bool retorno = false;
+            dt = db.BuscarDb(string.Format("Select *from Usuarios where Nombre = '" + Nombre + "' and Clave = '" + Clave + "'"));
+
+            if (dt.Rows.Count > 0)
+            {
+                retorno = true;
+
+            }
+            return retorno;
+
+        }
+
+        public bool Eliminar()
+        {
+            return db.EjecutarDB("Delete from Usuarios where Nombre = '" + Nombre + "'");
+        }
+
+        public bool Buscar()
+        {
+            bool retorno = false;
+            dt = db.BuscarDb("Select * from Usuarios where Nombre = '" + Nombre + "'");
+
+            if (dt.Rows.Count > 0)
+            {
+                Nombre = (string)dt.Rows[0]["Nombre"];
+                Clave = (string)dt.Rows[0]["Clave"];
+                Tipo = (int)dt.Rows[0]["Tipo"];
+                Activo = (bool)dt.Rows[0]["Activo"];
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        public DataTable Listar(string Parametro)
+        {
+            return db.BuscarDb("Select * from Usuarios where " + Parametro);
+        }
+
+        public bool Actualizar()
+        {
+            return db.EjecutarDB("Update Usuarios set Nombre = '" + Nombre + "', Clave = '" + Clave + "', Tipo = " + Tipo + ", Activo = '" + Activo + "'");
+        }
+    }
+}
