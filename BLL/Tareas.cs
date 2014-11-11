299e03036f50ce615c2105d5fc8fe69a7a90bca5
Anthony <Anthon_45@hotmail.com>
2014-11-11 13:38:28 -0500

    Tarea 21 : Asignacion Completa de Anthony Alberto de Tareas y Tareas detalle

---
+++ BLL/Tareas.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using DAL;
+
+namespace BLL
+{
+    public class Tareas
+    {
+        ConexionDb conexion = new ConexionDb();
+
+        public int IdTareas { get; set; }
+        public string CodigoTarea { get; set; }
+        public DateTime Fecha { get; set; }
+        public DateTime Vence { get; set; }
+        public int IdSemestre { get; set; }
+        public int IdAsignatura { get; set; }
+        public int IdSeccion { get; set; }
+        public string Descripcion { get; set; }
+
+
+
+        public int GetIdTarea(string codigotarea, DateTime fecha, DateTime vence, int idsemestre, int idasignatura, int idseccion, string descripcion)
+        {
+            DataTable dt = new DataTable();
+            dt = conexion.BuscarDb("Select IdTarea form Tareas where CodigoTarea = '"+  
+               codigotarea +"' and Fecha = '"+ fecha.ToString("yyyy/MM/dd") +"' and Vence = '"+ vence.ToString("yyyy/MM/dd")+"' and IdSemestre = '"+ idsemestre
+               +"' and IdAsignatura = '"+ idasignatura +"' and IdSeccion = '"+ IdSeccion +"' Descripcion = '"+ descripcion +"'  ");
+            if (dt.Rows.Count > 0)
+            {
+                this.IdTareas = (int)dt.Rows[0]["IdTarea"];
+            }
+            return this.IdTareas;
+        }
+
+
+        public bool Insertar()
+        {
+            bool paso = true;
+            paso = conexion.EjecutarDB("Insert into Tareas(CodigoTarea, Fecha, Vence, IdSemestre, IdAsignatura, IdSeccion, Descripcion) values ('" + CodigoTarea + "' , '"
+            + Fecha.ToString("yyyy/MM/dd") + "', '" + Vence.ToString("yyyy/MM/dd") + "', '" + IdSemestre + "', '" + IdAsignatura + "', '" + IdSeccion + "', '" + Descripcion + "')");
+
+            if (paso)
+            {
+                this.IdTareas = (int)conexion.ObtenerValorDb("select max (IdTarea) from Tareas");
+            }
+
+            return paso;
+        }
+
+
+        public bool Modificar()
+        {
+            return conexion.EjecutarDB("update Tareas set CodigoTarea = '" + CodigoTarea +
+                "', Fecha = '" + Fecha + "', Vence = '" + Vence + "', IdSemestre = '" + IdSemestre +
+                "', IdAsignatura = '" + IdAsignatura + "', IdSeccion = '" + IdSeccion + "', Descripcion = '" + Descripcion + "'");
+        }
+
+
+        public bool Eliminar()
+        {
+            return conexion.EjecutarDB("Delete from Tareas where IdTarea = " + IdTareas);
+        }
+
+
+        public bool Buscar(string Id)
+        {
+            bool mensaje = false;
+            DataTable dt = new DataTable();
+            dt = conexion.BuscarDb("Select * from Tareas where IdTarea = " + Id);
+
+            if (dt.Rows.Count > 0)
+            {
+                mensaje = true;
+                this.CodigoTarea = (string)dt.Rows[0]["CodigoTarea"];
+                this.Fecha = (DateTime)dt.Rows[0]["Fecha"];
+                this.Vence = (DateTime)dt.Rows[0]["Vence"];
+                this.IdSemestre = (int)dt.Rows[0]["IdSemestre"];
+                this.IdAsignatura = (int)dt.Rows[0]["IdAsignatura"];
+                this.IdSeccion = (int)dt.Rows[0]["IdSeccion"];
+                this.Descripcion = (string)dt.Rows[0]["Descripcion"];
+            }
+            return mensaje;
+        }
+    }
+}
---
+++ TeacherControl2/Presentacion/IniciarTareas.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BLL;
+
+namespace TeacherControl.Registro
+{
+    public partial class IniciarTareas : System.Web.UI.Page
+    {
+        Semestres semestre = new Semestres();
+
+        Asignaturas asignatura = new Asignaturas();
+
+        Secciones secciones = new Secciones();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                IdAsignaturaDropDownList.DataSource = asignatura.Listar("1=1");
+                IdAsignaturaDropDownList.DataValueField = "IdAsignatura";
+                IdAsignaturaDropDownList.DataTextField = "Nombre";
+                IdAsignaturaDropDownList.DataBind();
+
+
+                IdSeccionDropDownList.DataSource = secciones.Listar("1=1");
+                IdSeccionDropDownList.DataValueField = "IdSeccion";
+                IdSeccionDropDownList.DataTextField = "Numero";
+                IdSeccionDropDownList.DataBind();
+
+                IdSemestreDropDownList.DataSource = semestre.Listar("1=1");
+                IdSemestreDropDownList.DataValueField = "IdSemestre";
+                IdSemestreDropDownList.DataTextField = "Codigo";
+                IdSemestreDropDownList.DataBind();
+
+            }
+
+        }
+
+        protected void InciarButton_Click(object sender, EventArgs e)
+        {
+            Tareas tarea = new Tareas();
+
+            tarea.CodigoTarea = Convert.ToString(CodigoTareaTextBox.Text);
+            tarea.Fecha = Convert.ToDateTime(FechaTextBox.Text);
+            tarea.Vence = Convert.ToDateTime(VenceTextBox.Text);
+            tarea.IdSemestre = int.Parse(IdSemestreDropDownList.SelectedValue);
+            tarea.IdAsignatura = int.Parse(IdAsignaturaDropDownList.SelectedValue);
+            tarea.IdSeccion = int.Parse(IdSeccionDropDownList.SelectedValue);
+            tarea.Descripcion = Convert.ToString(DescripcionTextBox.Text);
+
+            if (tarea.Insertar())
+            {
+                Response.Redirect("PasarTareas.aspx?id=" + tarea.IdTareas);
+            }
+
+        }
+
+        protected void FechaTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
