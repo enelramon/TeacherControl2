21cac211e409f949f5da83b134be411e94777cb5
WcCastro123 <wagner_123_15@hotmail.com>
2014-11-11 15:51:29 -0400

    Agregando Formulario de consulta de Calificaciones

---
+++ TeacherControl2/Presentacion/ConsultaCalificaciones.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using TeacherControl.BLL;
+
+namespace TeacherControl.Consulta
+{
+    public partial class ConsultaCalificaciones : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Calificaciones calificaciones = new Calificaciones();
+            ConsultaGridView.DataSource = calificaciones.Listar("");
+            ConsultaGridView.DataBind();
+        }
+
+        protected void BuscarButton_Click(object sender, EventArgs e)
+        {
+            Calificaciones calificaciones = new Calificaciones();
+            string busqueda = BusquedaTextBox.Text;
+            ConsultaGridView.DataSource = calificaciones.Listar(busqueda);
+            ConsultaGridView.DataBind();
+        }
+
+
+    }
+}
