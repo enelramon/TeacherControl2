cd067e6bb2dac1e3e189621c36ca6b22fc81fccd
enelramon <enelramon@gmail.com>
2014-11-11 22:57:55 -0400

    Removiendo archivos sobrantes

--- BLL/Tareas.cs
+++ BLL/Tareas.cs
@@ -5,8 +5,10 @@ using System.Web;
 using System.Data;
 using DAL;
 
-namespace BLL {
-    public class Tareas {
+namespace BLL
+{
+    public class Tareas
+    {
 
         public int IdTarea { set; get; }
         public string CodigoTarea { set; get; }
@@ -19,26 +21,31 @@ namespace BLL {
 
         ConexionDb conexion = new ConexionDb();
 
-        public bool Insertar() {
+        public bool Insertar()
+        {
             bool paso = conexion.EjecutarDB("insert into Tareas(CodigoTarea, Fecha, Vence, IdSemestre, IdAsignatura, Descripcion, ResultadoEsperado) values(" + CodigoTarea.ToDbString() + "," + Fecha.ToDbString() + "," + Vence.ToDbString() + "," + IdSemestre + "," + IdAsignatura + "," + Descripcion.ToDbString() + "," + ResultadoEsperado.ToDbString() + ")");
             if (paso)
                 this.IdTarea = (int)conexion.ObtenerValorDb("select MAX(IdTarea) from Tareas");
             return paso;
         }
 
-        public bool Eliminar() {
+        public bool Eliminar()
+        {
             return conexion.EjecutarDB("Delete from Tareas where IdTarea = " + IdTarea);
         }
 
-        public bool Modificar() {
+        public bool Modificar()
+        {
             return conexion.EjecutarDB("Update Tareas set CodigoTarea = " + CodigoTarea.ToDbString() + ", Fecha = " + Fecha.ToDbString() + ", Vence = " + Vence.ToDbString() + ", IdSemestre = " + IdSemestre + ", IdAsignatura = " + IdAsignatura + ", Descripcion = " + Descripcion.ToDbString() + ", ResultadoEsperado = " + ResultadoEsperado.ToDbString() + " where IdTarea = " + IdTarea);
         }
 
-        public bool Buscar(int Id) {
+        public bool Buscar(int Id)
+        {
             bool mensaje = false;
             DataTable dt;
             dt = conexion.BuscarDb("select * from Tareas where IdTarea = " + Id);
-            if (dt.Rows.Count > 0) {
+            if (dt.Rows.Count > 0)
+            {
                 mensaje = true;
                 DataRow row = dt.Rows[0];
                 this.IdTarea = (int)row["IdTarea"];
@@ -53,12 +60,14 @@ namespace BLL {
             return mensaje;
         }
 
-        public static DataTable Listar(string condicion) {
+        public static DataTable Listar(string condicion)
+        {
             ConexionDb conexion = new ConexionDb();
             return conexion.BuscarDb("select * from Tareas where " + condicion);
         }
 
-        public static DataTable Listar(string columnas, string condicion) {
+        public static DataTable Listar(string columnas, string condicion)
+        {
             ConexionDb conexion = new ConexionDb();
             return conexion.BuscarDb("select " + columnas + " from Tareas where " + condicion);
         }
