b6e9e1e328dd3c4e7b4e599fde8c7302b3b16cf5
Anthony <Anthon_45@hotmail.com>
2014-11-16 18:40:39 -0500

    clase profesores y secciones agregadas

---
+++ BLL/Profesores.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using DAL;
+
+namespace BLL
+{
+    public class Profesores
+    {
+        ConexionDb conexion = new ConexionDb();
+
+        #region "Propiedades"
+        public int IdProfesor { set; get; }
+        public string Nombres { set; get; }
+        public string Apellidos { set; get; }
+        public string Direccion { set; get; }
+        public int Genero { set; get; }
+        public DateTime FechaNacimiento { set; get; }
+        public string Email { set; get; }
+        public string Telefono1 { set; get; }
+        public string Telefono2 { set; get; }
+        #endregion
+
+        #region "Metodos"
+        public bool Insertar()
+        {
+            return conexion.EjecutarDB("Insert into Profesores( Nombres, Apellidos, Direccion,Genero,FechaNacimiento,Email,Telefono1,Telefono2)values('" 
+                + Nombres + "','" + Apellidos + "','" + Direccion + "','" + Genero + "','" + FechaNacimiento.ToString("MM/dd/yyyy") +
+                "','" + Email + "','" + Telefono1 + "','" + Telefono2 + "')");
+        }
+
+        public bool Modificar()
+        {
+            return conexion.EjecutarDB("update Profesores set Nombres='" + Nombres + "',Apellidos='" +
+                Apellidos + "',Direccion='" + Direccion + "',Genero='" + Genero + "',FechaNacimiento='" + 
+                FechaNacimiento.ToString("MM/dd/yyyy") + "',Email='" + Email + "',Telefono1='" + Telefono1 +
+                "',Telefono2='" + Telefono2 + "' where IdProfesor=" + IdProfesor);
+
+        }
+
+
+        public bool Eliminar(int IdProfesor)
+        {
+            return conexion.EjecutarDB("Delete from Profesores where IdProfesor =" + IdProfesor);
+        }
+
+
+        public bool Buscar(string prmNombre)
+        {
+            bool Retorno = false;
+            DataTable Datos = new DataTable();
+            Datos = conexion.BuscarDb("Select * From Profesores where Nombres = '" + prmNombre + "'");
+            if (Datos.Rows.Count > 0)
+            {
+                Retorno = true;
+                this.IdProfesor = (int)Datos.Rows[0]["IdProfesor"];
+                this.Nombres = (string)Datos.Rows[0]["Nombres"];
+                this.Apellidos = (string)Datos.Rows[0]["Apellidos"];
+                this.Direccion = (string)Datos.Rows[0]["Direccion"];
+                this.Genero = Convert.ToInt32((byte)Datos.Rows[0]["Genero"]);
+                this.FechaNacimiento = (DateTime)Datos.Rows[0]["FechaNacimiento"];
+                this.Email = (string)Datos.Rows[0]["Email"];
+                this.Telefono1 = (string)Datos.Rows[0]["Telefono1"];
+                this.Telefono2 = (string)Datos.Rows[0]["Telefono2"];    
+            }
+            return Retorno;
+        }
+
+        public DataTable Listar(string filtroWhere)
+        {
+
+            return conexion.BuscarDb("Select * From Profesores where " + filtroWhere);
+
+        }
+
+
+
+        public DataTable Listar()
+        {
+            DataTable dt = new DataTable();
+            dt = conexion.BuscarDb("Select * From Profesores");
+            return dt;
+        }
+        #endregion
+    }
+}
---
+++ BLL/Secciones.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using DAL;
+
+namespace BLL
+{
+    public class Secciones
+    {
+        ConexionDb conexion = new ConexionDb();
+
+        #region "Propiedades"
+        public int IdSeccion { set; get; }
+        public int Numero { set; get; }
+        public int IdAsignatura { set; get; }
+        public int IdProfesor { set; get; }
+        public string Aula { set; get; }
+        public string HoraInicio { set; get; }
+        public string HoraFin { set; get; }
+        public bool Activo { set; get; }
+        #endregion
+
+        #region "Metodos"
+
+        public bool Insertar()
+        {
+            return conexion.EjecutarDB("Insert into Secciones(Numero, IdAsignatura, IdProfesor, Aula,HoraInicio,HoraFin,Activa) values ('" + Numero +
+                "','" + IdAsignatura + "','" + IdProfesor + "','" + Aula + "','" + HoraInicio + "','" + HoraFin + "','" + true + "')");
+        }
+
+        public bool Modificar()
+        {
+            return conexion.EjecutarDB("update Secciones set Numero ='" + Numero +
+                "',IdAsignatura ='" + IdAsignatura + "',IdProfesor ='" + IdProfesor +
+                "',Aula ='" + Aula + "',HoraInicio ='" + HoraInicio +
+                "',HoraFin ='" + HoraFin + "' where IdSeccion = " + IdSeccion);
+        }
+
+
+        public bool Eliminar(string matricula)
+        {
+            return conexion.EjecutarDB("Delete from Secciones where IdSeccion = " + IdSeccion);
+        }
+
+
+        public bool Buscar(int NumeroSeccion)
+        {
+            bool mensaje = false;
+            DataTable dt = new DataTable();
+            dt = conexion.BuscarDb("Select * From Secciones where Numero = " + NumeroSeccion);
+            if (dt.Rows.Count > 0)
+            {
+                mensaje = true;
+                this.IdSeccion = (int)dt.Rows[0]["IdSeccion"];
+                this.Numero = (int)dt.Rows[0]["Numero"];
+                this.IdAsignatura = (int)dt.Rows[0]["IdAsignatura"];
+                this.IdProfesor = (int)dt.Rows[0]["IdProfesor"];
+                this.Aula = (string)dt.Rows[0]["Aula"];
+                this.HoraInicio = (string)dt.Rows[0]["HoraInicio"];
+                this.HoraFin = (string)dt.Rows[0]["HoraFin"];
+                this.Activo = (bool)dt.Rows[0]["Activa"];
+            }
+            return mensaje;
+        }
+
+        public DataTable Listar(string filtro)
+        {
+            DataTable dt = new DataTable();
+            dt = conexion.BuscarDb("Select * From Secciones where " + filtro);
+            return dt;
+        }
+
+
+        public DataTable Listar()
+        {
+            DataTable dt = new DataTable();
+            dt = conexion.BuscarDb("Select * From Secciones");
+            return dt;
+        }
+        #endregion
+    }
+}
